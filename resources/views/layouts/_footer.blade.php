063de51a16756aba51302e19fc21ef42af7fb674
jz560 <jzz08111@gmail.com>
2020-03-11 13:02:54 +0000

    basic pages structure

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Author - JZ</a> <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+    <p class="float-right"><a href="mailto:jz08111@gmail.com">联系我们</a></p>
+  </div>
+</footer>
