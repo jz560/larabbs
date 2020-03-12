72ee403ec6d5fadf4c9edf62a9d53fd1bcf2d7ae
jz560 <jzz08111@gmail.com>
2020-03-12 15:00:01 +0000

    email vertification

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,38 +2,40 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable;
+    use Notifiable, MustVerifyEmailTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function register(Request $request)
+    {
+        // 检验用户提交的数据是否有误
+        $this->validator($request->all())->validate();
+
+        // 创建用户同时触发用户注册成功的事件，并将用户传参
+        event(new Registered($user = $this->create($request->all())));
+
+        // 登录用户
+        $this->guard()->login($user);
+
+        // 调用钩子方法 `registered()` 
+        return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+    }
 }
