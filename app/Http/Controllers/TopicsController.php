adedd46c8c0c19b2373b3eda70213f4dc64be36d
jz560 <jzz08111@gmail.com>
2020-03-19 15:43:35 +0000

    publish replies

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -25,8 +25,13 @@ class TopicsController extends Controller
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Topic $topic)
+	public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -90,14 +95,4 @@ class TopicsController extends Controller
         }
         return $data;
 	}
-	
-	public function show(Request $request, Topic $topic)
-    {
-        // URL 矫正
-        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
-            return redirect($topic->link(), 301);
-        }
-
-        return view('topics.show', compact('topic'));
-    }
 }
